3f0f3aa39ccd2f568848091a0644d415cf4fa7f6
Michelle Ranly <meranly8@gmail.com>
2021-02-16 02:48:48 -0600

    Created and migrated Users table

---
+++ db/migrate/20210216084652_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :uid
+      t.string :provider
+
+      t.timestamps
+    end
+  end
+end
