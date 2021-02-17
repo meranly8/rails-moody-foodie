1964ada51979da2130135a88b22189058647cae7
Michelle Ranly <meranly8@gmail.com>
2021-02-17 00:27:42 -0600

    Added dependent: :delete_all to Entry has_many :entry_provisions relationship

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,6 +2,6 @@ class Entry < ApplicationRecord
     validates :date, presence: true
 
     belongs_to :user
-    has_many :entry_provisions
+    has_many :entry_provisions, dependent: :delete_all
     has_many :provisions, through: :entry_provisions
 end
