f63db65a7be18956c57dc0cf12276a7490b2c97a
Michelle Ranly <meranly8@gmail.com>
2021-02-17 00:23:05 -0600

    Added dependent: :delete_all to User has_many :entries relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     
-    has_many :entries
+    has_many :entries, dependent: :delete_all
 
     validates :name, presence: true
     validates :email, presence: true
