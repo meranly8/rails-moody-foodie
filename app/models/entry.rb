f8fee46dafdc29d25da773a98ae16a5338dc3528
Michelle Ranly <meranly8@gmail.com>
2021-02-16 22:07:51 -0600

    Added :user, :entry_provisions, :provisions associations to Entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,7 @@
 class Entry < ApplicationRecord
     validates :date, presence: true
+
+    belongs_to :user
+    has_many :entry_provisions
+    has_many :provisions, through: :entry_provisions
 end
