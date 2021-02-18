41eeb8f5b07ed0ab0cf527fed4fdead4c9a7deb8
Michelle Ranly <meranly8@gmail.com>
2021-02-18 00:43:37 -0600

    Defined users#create action and built #user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:email, :password, :password_confirmation)
+        end
 end
