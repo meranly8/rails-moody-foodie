f409eb22405fa8aef35c653c72f23636059cec19
Michelle Ranly <meranly8@gmail.com>
2021-02-17 23:18:58 -0600

    Drew User routes to :new, :create, :show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/users/:id', to: 'users#show'
 end
