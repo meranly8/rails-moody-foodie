74f073e7bedaec4074779337496b9f5051139526
Michelle Ranly <meranly8@gmail.com>
2021-02-18 01:46:40 -0600

    Added login logic to users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.valid?
+        if @user.save
+            session[:user_id] = @user.id
             redirect_to root_path
         else
             flash[:message] = @user.errors.full_messages.join("")
