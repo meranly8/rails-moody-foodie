dc515088f3af077c9a77d74c7657dde87bab543a
Michelle Ranly <meranly8@gmail.com>
2021-02-17 22:29:54 -0600

    Added Provision associations to :entry_provisions and :entries

--- app/models/provision.rb
+++ app/models/provision.rb
@@ -1,4 +1,7 @@
 class Provision < ApplicationRecord
     validates :name, presence: true
     validates :provision_type, presence: true
+
+    has_many :entry_provisions
+    has_many :entries, through: :entry_provisions
 end
