234ee2a91d124d8a06c05279569631348ad6426c
Michelle Ranly <meranly8@gmail.com>
2021-02-17 23:22:49 -0600

    Added 'user' alias to users#show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  get '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#show', as: 'user'
 end
