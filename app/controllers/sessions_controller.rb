a924d4de88bf9521b132d53c51aa1e908cc1b8af
Michelle Ranly <meranly8@gmail.com>
2021-02-18 20:34:30 -0600

    Added authenticate and login logic to sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(params[:id])
+        if user && user.authenticate(params[:password])
+            session[:id] = user.id
+            redirect_to user_path(user)
+        else
+            redirect_to login_path
+        end
     end
 
     def destroy
