175a374eb1eb94ad92331e54d13dbf8e42d12e4f
Michelle Ranly <meranly8@gmail.com>
2021-02-19 00:30:32 -0600

    Built User.find_or_create_from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,11 @@ class User < ApplicationRecord
     validates :email, presence: true
     validates :email, uniqueness: true
 
-    
+    def self.find_or_create_from_omniauth(auth)
+        User.find_or_create_by(uid: auth['uid'], provider: ['provider']) do |u|
+            u.name = auth['info']['first_name']
+            u.email = auth['info']['email']
+            u.password = SecureRandom.hex(16)
+        end
+    end
 end
