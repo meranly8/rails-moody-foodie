e75ed26e1454e1e240a74e1ea96d5eaa0325611f
Michelle Ranly <meranly8@gmail.com>
2021-02-18 21:06:57 -0600

    Updated users#create to find_by email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(params[:id])
+        user = User.find_by(email: params[:user][:email])
         if user && user.authenticate(params[:password])
             session[:id] = user.id
             redirect_to user_path(user)
