aaa8790a5158d565fc004675afb008915093bd96
Michelle Ranly <meranly8@gmail.com>
2021-02-18 22:20:39 -0600

    Defined #current_user and #logged_in? app helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        @current_user = User.find_by(id: session[:user]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
