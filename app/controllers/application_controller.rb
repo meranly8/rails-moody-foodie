03c760ef7ef97ab2bc7be1044a22d867cad3c9a3
Michelle Ranly <meranly8@gmail.com>
2021-02-21 11:09:14 -0600

    Updated @current_user helper method to use memoization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     def current_user
-        @current_user = User.find_by(id: session[:user]) if session[:user_id]
+        @current_user ||= User.find_by(id: session[:user]) if session[:user_id]
     end
 
     def logged_in?
