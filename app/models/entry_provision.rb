41bf95d7ced6ce74d5596c667206b6682e0f6a2f
Michelle Ranly <meranly8@gmail.com>
2021-02-20 22:52:12 -0600

    Built EntryProvision method for displaying #amount_and_unit

--- app/models/entry_provision.rb
+++ app/models/entry_provision.rb
@@ -5,4 +5,8 @@ class EntryProvision < ApplicationRecord
     validates :amount, presence: true
     validates :amount, numericality: {greater_than: 0}
     validates :unit, presence: true
+
+    def amount_and_unit
+        "#{self.amount} #{self.unit}"
+    end
 end
