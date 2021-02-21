304f7393ba25c11b2d5635fca9279b5034531ab6
Michelle Ranly <meranly8@gmail.com>
2021-02-20 22:50:01 -0600

    Built Entry methods for displaying #date_and_mood and #short_date

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,4 +4,13 @@ class Entry < ApplicationRecord
     belongs_to :user
     has_many :entry_provisions, dependent: :delete_all
     has_many :provisions, through: :entry_provisions
+
+    def date_and_mood
+        "#{self.date.strftime("%A, %d %b %Y")} - #{self.end_of_day_mood}"
+    end
+
+    def short_date
+        self.date.strftime("%m/%d/%Y")
+    end
+
 end
