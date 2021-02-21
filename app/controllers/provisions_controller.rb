ece6068fd6297b24364c6acd4d2d92324f6e5047
Michelle Ranly <meranly8@gmail.com>
2021-02-20 18:25:45 -0600

    Defined provisions#index and provisions#show

--- app/controllers/provisions_controller.rb
+++ app/controllers/provisions_controller.rb
@@ -1,3 +1,9 @@
 class ProvisionsController < ApplicationController
-    
+    def index
+        @provisions = Provision.all
+    end
+
+    def show
+        @provision = Provision.find_by(id: params[:id])
+    end
 end
