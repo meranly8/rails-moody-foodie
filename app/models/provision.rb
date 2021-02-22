5a880acc513bd5f07af24fec4eee37b724f1d213
Michelle Ranly <meranly8@gmail.com>
2021-02-21 22:04:00 -0600

    Added provision presence validations onto one line

--- app/models/provision.rb
+++ app/models/provision.rb
@@ -1,6 +1,5 @@
 class Provision < ApplicationRecord
-    validates :name, presence: true
-    validates :provision_type, presence: true
+    validates :name, :provision_type, presence: true
 
     has_many :entry_provisions
     has_many :entries, through: :entry_provisions
