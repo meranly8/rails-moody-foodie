5cdcb011f2631038c1fa0e2a98c8a753bfea890c
Michelle Ranly <meranly8@gmail.com>
2021-02-21 21:58:57 -0600

    Added entry_provision presence validations onto one line

--- app/models/entry_provision.rb
+++ app/models/entry_provision.rb
@@ -2,9 +2,8 @@ class EntryProvision < ApplicationRecord
     belongs_to :entry
     belongs_to :provision
 
-    validates :amount, presence: true
+    validates :amount, :unit, presence: true
     validates :amount, numericality: {greater_than: 0}
-    validates :unit, presence: true
 
     def amount_and_unit
         "#{self.amount} #{self.unit}"
