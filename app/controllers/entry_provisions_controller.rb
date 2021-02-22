ce2b4065cf7cdf1b18e3abda7ba9f94785b727df
Michelle Ranly <meranly8@gmail.com>
2021-02-21 21:00:10 -0600

    Defined entry_provisions#update and #entry_provision_params

--- app/controllers/entry_provisions_controller.rb
+++ app/controllers/entry_provisions_controller.rb
@@ -13,4 +13,15 @@ class EntryProvisionsController < ApplicationController
   def edit
     @entry_provision = EntryProvision.find_by(id: params[:id])
   end
+
+  def update
+    @entry_provision = EntryProvision.find_by(id: params[:id])
+    @entry_provision.update(entry_provision_params)
+    redirect_to entry_provision_path(@entry_provision)
+  end
+
+  private
+    def entry_provisions_params
+      params.require(:entry_provision).permit(:provision_id, :amount, :unit, :homemade, :description)
+    end
 end
