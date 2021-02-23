d019870ab28a7f50acf2021ad45ab2ac565fb0cd
Michelle Ranly <meranly8@gmail.com>
2021-02-22 21:08:00 -0600

    Defined provisions#create and #provision_params

--- app/controllers/provisions_controller.rb
+++ app/controllers/provisions_controller.rb
@@ -10,4 +10,19 @@ class ProvisionsController < ApplicationController
     def new
         @provision = Provision.new
     end
+
+    def create
+        @provision = Provision.new(provision_params)
+        if @provision.save
+            redirect_to provision_path(@provision)
+        else
+            flash[:message] = @provision.errors.full_messages.join("")
+            render :new
+        end
+    end
+
+    private
+        def provision_params
+            params.require(:provision).permit(:name, :provision_type, :approx_cals_per_serving)
+        end
 end
