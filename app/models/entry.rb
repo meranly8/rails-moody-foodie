e2cda4b7fa22392ec0dedcca65d97dd446704216
Michelle Ranly <meranly8@gmail.com>
2021-02-22 20:03:19 -0600

    Added :date uniqueness validation into Entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,6 @@
 class Entry < ApplicationRecord
     validates :date, presence: true
+    validates :date, uniqueness: true
 
     belongs_to :user
     has_many :entry_provisions, dependent: :delete_all
