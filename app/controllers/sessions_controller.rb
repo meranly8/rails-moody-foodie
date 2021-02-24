adfe4dc902dac1bf314054d0d5b71eb1b031c4f0
Michelle Ranly <meranly8@gmail.com>
2021-02-24 02:33:44 -0600

    Updated sessions#create session[key] to be :user_id and updated flash[:message] value

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
-            session[:id] = user.id
+            session[:user_id] = user.id
             redirect_to user_path(user)
         else
-            flash[:message] = "Invalid credentials. Please try again."
+            flash[:message] = "Invalid credentials"
             redirect_to login_path
         end
     end
