503f38531930b1c76d84b711e7b6a925f93bb998
Michelle Ranly <meranly8@gmail.com>
2021-02-25 01:07:49 -0600

    Added name uniqueness validation in Provision

--- app/models/provision.rb
+++ app/models/provision.rb
@@ -1,5 +1,6 @@
 class Provision < ApplicationRecord
     validates :name, :provision_type, presence: true
+    validates :name, uniqueness: true
 
     has_many :entry_provisions
     has_many :entries, through: :entry_provisions
