955a7eb9bd0f39c973048dfd9e07411f13a45278
Michelle Ranly <meranly8@gmail.com>
2021-02-25 16:01:42 -0600

    Defined #authorized_for? application helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user, :logged_in?, :enforce_login
+    helper_method :current_user, :logged_in?, :enforce_login, :authorized_for
 
     def current_user
         @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -11,5 +11,9 @@ class ApplicationController < ActionController::Base
 
     def enforce_login
         return redirect_to login_path unless logged_in?
+    end
+
+    def authorized_for?(entry)
+        entry.user == current_user
     end
 end
