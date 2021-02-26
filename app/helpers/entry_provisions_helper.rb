90ddc1cf1891a5889e4287a0f61859d463bc96c2
Michelle Ranly <meranly8@gmail.com>
2021-02-26 01:20:27 -0600

    Reformatted EntryProvisionsHelper #display_header; moved #amount_and_unit into helper

--- app/helpers/entry_provisions_helper.rb
+++ app/helpers/entry_provisions_helper.rb
@@ -1,5 +1,9 @@
 module EntryProvisionsHelper
     def display_header(entry)
-        tag.h1("All #{entry.date} Provisions")
+        tag.h1("#{entry.short_date} Provisions")
+    end
+
+    def amount_and_unit
+        "#{self.amount} #{self.unit}"
     end
 end
