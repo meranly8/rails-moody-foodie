a4d60a1bff52f6597d0d49bce1638a31a3a7d047
Michelle Ranly <meranly8@gmail.com>
2021-02-27 02:32:31 -0600

    Defined #date_and_mood and #short_date in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
     def amount_and_unit
         "#{self.amount}  #{self.unit}"
     end
+
+    def date_and_mood
+        "#{self.date.strftime("%A %d %b %Y")} - #{self.end_of_day_mood}"
+    end
+
+    def short_date
+        self.date.strftime("%m-%d-%Y")
+    end
 end
