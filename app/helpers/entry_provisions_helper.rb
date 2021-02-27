a80f0ed235b204377d83baec7cbc9a94843029c6
Michelle Ranly <meranly8@gmail.com>
2021-02-27 01:11:40 -0600

    Defined #calorie_math() and #calorie_total() entry provisions helper

--- app/helpers/entry_provisions_helper.rb
+++ app/helpers/entry_provisions_helper.rb
@@ -1,9 +1,23 @@
 module EntryProvisionsHelper
     def display_header(entry)
-        tag.h1("#{entry.short_date} Provisions")
+        tag.h1("#{entry.short_date} Provisions Calorie Details")
     end
 
     def amount_and_unit
         "#{self.amount} #{self.unit}"
     end
+
+    def calorie_math(amount, cals)
+        amount * cals
+    end
+
+    def calorie_total(entry_provisions)
+        @cal_sum = 0
+        entry_provisions.each do |ep|
+            @cal_sum += ep.provision.approx_cals_per_serving
+        end
+
+        @cal_sum
+    end
+    
 end
