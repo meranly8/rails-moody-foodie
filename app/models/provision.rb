91a3af43e664924a5eab231f9b2006e4db22d898
Michelle Ranly <meranly8@gmail.com>
2021-02-28 17:52:58 -0600

    Added presence and numericality validations for approx_cals_per_serving

--- app/models/provision.rb
+++ app/models/provision.rb
@@ -1,6 +1,7 @@
 class Provision < ApplicationRecord
-    validates :name, :provision_type, presence: true
+    validates :name, :provision_type, :approx_cals_per_serving, presence: true
     validates :name, uniqueness: true
+    validates :approx_cals_per_serving, numericality: {greater_than: -1}
 
     has_many :entry_provisions
     has_many :entries, through: :entry_provisions
