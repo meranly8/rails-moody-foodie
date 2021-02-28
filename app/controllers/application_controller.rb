85aa90ed8956a28a6d025394db962aef6b18c0cd
Michelle Ranly <meranly8@gmail.com>
2021-02-28 17:44:05 -0600

    Included ApplicationHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?, :enforce_login, :authorized_for?
-
+    include ApplicationHelper
+    
     def current_user
         @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
